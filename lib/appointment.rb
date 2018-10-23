98c072cb82dc5a97cd0f9f80a582886f33c9e78f
Mike Dilley <mike.dilley@gmail.com>
2018-10-23 22:50:46 +0000

    automatically backed up by learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -8,10 +8,15 @@ class Appointment
     @patient = patient
     @doctor = doctor
     @date = date
+    @@all << self
   end
 
   def self.all
     @@all
+  end
+
+  def appointments
+
   end
 
 end
